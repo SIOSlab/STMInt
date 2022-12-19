Fixed test cases and error in skew computation.

tests/test_STMint.py
@@ -59,7 +59,7 @@ class TestTwoBodyMotion(unittest.TestCase):
         stt2_reference = self.testDynamicsAndVariational2_20[2]
         stt2_cocycle = self.presetTest.cocycle2(self.testDynamicsAndVariational2_10[1], self.testDynamicsAndVariational2_10[2], 
                             self.testDynamicsAndVariational2_21[1], self.testDynamicsAndVariational2_21[2])[1]
-        self.assertTrue(np.amax(np.abs((stt2_cocycle-stt2_reference))) < 10.**(-8))
+        self.assertTrue(np.amax(np.abs((stt2_cocycle-stt2_reference))) < 1E-8)
 
 
     def test_propagation(self):
@@ -88,7 +88,6 @@ class TestTwoBodyMotion(unittest.TestCase):
 
         self.assertTrue((((NumericalDeltaX_f-IVPdeltaX_f).norm())/NumericalDeltaX_f.norm()) < .02)
 
-
     def test_propagationWithFindSTM(self):
 
         # Calculating STM from findSTM
@@ -109,9 +108,8 @@ class TestTwoBodyMotion(unittest.TestCase):
         for i in range(len(self.testDynamicsAndVariational1.y)):
             t_f.append(self.testDynamicsAndVariational1.y[i][-1])
 
-        stmSTMint = Matrix(np.reshape(t_f[6:], (6,6)))
-
-        self.assertTrue((np.linalg.norm(stmUtil-stmSTMint)/np.linalg.norm(stmUtil)) < .02)
+        stmSTMint = np.reshape(t_f[6:], (6,6))
+        self.assertTrue((np.linalg.norm(stmUtil-stmSTMint)/np.linalg.norm(stmUtil)) < 1E-8)
 
 
 class TestThreeBodyMotion(unittest.TestCase):

tests/util.py
@@ -12,11 +12,12 @@ def skew(vec):
 		mat (numpy array (3x3)):
 			The cross product matrix associated with vec
 	"""
-	return np.array([[0., -1.*vec[2], vec[1]],[vec[2],0.,-1.*vec[0]],[-1.*vec[1], vec[0], 0.]])*(vec[[0]])
+	return np.array([[0., -1.*vec[2], vec[1]],[vec[2],0.,-1.*vec[0]],[-1.*vec[1], vec[0], 0.]])
 
 def findSTM(r0, v0, rf, vf, dt):
 	"""
 	Return the state transition matrix associated with this trajectory
+	https://doi.org/10.2514/1.G006373 Reid Reynolds - Direct Solution of the Keplerian State Transition Matrix
 
 	Args:
 		r0 (numpy array (3)):
@@ -43,7 +44,6 @@ def findSTM(r0, v0, rf, vf, dt):
 	svf = skew(vf)
 	sh = skew(h)
 	B=np.transpose(np.vstack([r0/np.sqrt(mu*r0Mag), r0Mag*v0/mu]))
-	Y0 = np.block([[sr0, -1.*np.matmul((np.matmul(sr0, sv0)+sh), B), -1.*np.transpose([r0])],[sv0, np.matmul(mu/r0Mag**3*np.matmul(sr0,sr0)-np.matmul(sv0,sv0), B), np.transpose([v0])/2.]])
+	Y0 = np.block([[sr0, -1.*np.matmul((np.matmul(sr0, sv0)+sh), B), -1.*np.transpose([r0])],[sv0, np.matmul(mu/r0Mag**3*np.matmul(sr0,sr0)-np.matmul(sv0,sv0), B), np.transpose([v0])/2.]])	
 	Yf = np.block([[srf, -1.*np.matmul((np.matmul(srf, svf)+sh), B), np.transpose([-1.*rf+3./2.*dt*vf])],[svf, np.matmul(mu/rfMag**3*np.matmul(srf,srf)-np.matmul(svf,svf), B), np.transpose([vf/2.-3./2.*mu/rfMag**3*dt*rf])]])
-	print(Y0)
 	return np.matmul(Yf, np.linalg.inv(Y0))