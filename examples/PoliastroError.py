b18b9d604971829d323b6503aab261eaf1e4bb19
Cedric Orton-Urbina <ceo29@cornell.edu>
2023-08-30 12:56:38 -0400

    First version of PoliastroError

--- examples/PoliastroError.py
+++ examples/PoliastroError.py
@@ -19,35 +19,29 @@ nu = 0 << u.deg
 iss_orbit = Orbit.from_classical(body.Earth, a, ecc, inc, raan, argp, nu)
 
 # ISS ICS
-x_0 = np.array([*iss_orbit.r, *iss_orbit.v])
+x_0 = np.array([*iss_orbit.r.value, *iss_orbit.v.value])
 
+def calc_error(stm, transfer_time, x_0, perturbation):
 
-# Step 1
-def calc_delta_v_0(integrator, transfer_time, x_0, delta_r_f_star):
-    phi = integrator.dynVar_int2([0, transfer_time], x_0, output="final")[1]
+    iss_reference_orbit = iss_orbit.propagate(transfer_time * u.s)
 
-    phi_rv = phi[:3, 3:]
+    r_f_ref = np.array([*iss_reference_orbit.r.value])
 
-    phi_rv_inv = np.linalg.inv(phi_rv)
+    delta_x_0 = np.array([0, 0, 0, *perturbation])
 
-    return np.matmul(phi_rv_inv, delta_r_f_star)
+    y_0 = x_0 + delta_x_0
 
-# Step 2
+    iss_perturbed_orbit = Orbit.from_vectors(body.Earth, y_0[:3] * u.km, y_0[3:] * u.km / u.s)
 
-def calc_delta_r_f_error(integrator, transfer_time, x_0, delta_r_f_star):
-    v_0 = calc_delta_v_0(integrator, transfer_time, x_0, delta_r_f_star)
+    r_f_pert = np.array([iss_perturbed_orbit.propagate(transfer_time * u.s).r.value])
 
-    r_f = integrator.dynVar_int([0, transfer_time], x_0, output="final")[0][:3]
-
-    delta_x_0 = x_0 + np.array([0, 0, 0, *v_0])
-
-    delta_r_f = integrator.dynVar_int([0, transfer_time], delta_x_0, output="final")[0][:3] - r_f
-
-    return np.linalg.norm((delta_r_f_star - delta_r_f), ord=2)
+    print((r_f_ref - r_f_pert))
+    print(np.matmul(stm, delta_x_0)[:3])
+    return np.linalg.norm(((r_f_ref - r_f_pert) - np.matmul(stm, delta_x_0)[:3]), ord=2)
 
 # Step 3
 
-def calc_sphere_max_error(integrator, transfer_time, x_0, r, n):
+def calc_sphere_max_error(stm, transfer_time, x_0, r, n):
     samples = np.random.multivariate_normal([0, 0, 0], np.identity(3), n)
 
     normalized_samples = []
@@ -58,19 +52,21 @@ def calc_sphere_max_error(integrator, transfer_time, x_0, r, n):
     errors = []
 
     for sample in normalized_samples:
-        errors.append(calc_delta_r_f_error(integrator, transfer_time, x_0, sample))
+        errors.append(calc_error(stm, transfer_time, x_0, sample))
 
     return max(errors)
 
 yvals = []
 xvals = []
-integrator = STMint(preset="twoBodyEarth", variational_order=2)
+integrator = STMint(preset="twoBodyEarth")
+transfer_time = iss_orbit.period.to(u.s).value/10.0
+stm = integrator.dynVar_int([0, transfer_time], x_0, output="final")[1]
 
 for i in range(100, 1000, 50):
-    r = x_0[2] / (1100.0 - i)
+    r = np.linalg.norm(x_0[3:]) / (1100.0 - i)
     xvals.append(r)
     n = 500
-    yvals.append(calc_sphere_max_error(integrator, iss_orbit.period/10.0, x_0, r, n))
+    yvals.append(calc_sphere_max_error(stm, transfer_time, x_0, r, n))
     print(str((i - 50) / 50) + " completed")
 
 plt.figure()
